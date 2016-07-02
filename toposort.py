6f2947ce42ae6cf6cf5db4da654804ef721f794a
maliknur <malik.dba.pro@gmail.com>
2016-07-02 10:41:22 -0700

    topological ordering of directed graph

---
+++ toposort.py
@@ -0,0 +1,41 @@
+# Compute a topological ordering of a given directed acyclic graph (DAG) 
+# with n vertices and m edges.
+
+#Uses python3
+
+import sys
+
+def DFS(adj, used, order, x):
+    if used[x] == 1:
+        return
+
+    used[x] = 1
+
+    for e in adj[x]:
+        DFS(adj, used, order, e)
+
+    order.append(x)
+
+
+
+def toposortGraph(adj):
+    used = [0] * len(adj)
+    order = []
+    for v in range(len(adj)):
+        DFS(adj, used, order, v)
+    return reversed(order)
+
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    n, m = data[0:2]
+    data = data[2:]
+    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    adj = [[] for _ in range(n)]
+    for (a, b) in edges:
+        adj[a - 1].append(b - 1)
+    order = toposortGraph(adj)
+    for x in order:
+        print(x + 1, end=' ')
